8b36325fcb7867ab6aee6f2298b25709783bb31c
jihoon1026 <53975530+jjinjun1026@users.noreply.github.com>
2023-05-21 01:02:42 +0900

    Update Main.java

--- src/com/kmit/www/Main.java
+++ src/com/kmit/www/Main.java
@@ -1,10 +1,10 @@
-package com.kmit.www;
+package com.kmit.www; // 패키지를 "com.kmit.www"로 정의 함
 
-import java.util.Scanner;
-    public class Main{
-        public static void main(String args[]){
-            System.out.println("이름, 도시, 나이, 키, 체중, 독신 여부를" + "빈칸으로 분리하여 입력하세요.");
-            Scanner scanner = new Scanner(System.in);
+import java.util.Scanner; // "java.util" 패키지에 속한 'Scanner' 클래스를 가져오는 코드
+    public class Main { // 주 클래스를 "Main"로 정의 함
+        public static void main(String args[]) { // 시작 구문
+            System.out.println("이름, 도시, 나이, 키, 체중, 독신 여부를 " + "빈칸으로 분리하여 입력하세요."); // 괄호 안의 문자를 출력합니다. (여기서 + 는 두개의 문장을 하나의 출력 문장으로 결합시켜주는 연산자이다.) 
+            Scanner scanner = new Scanner(System.in); //
 
             String name = scanner.next();
             System.out.print("이름 =" + name + ",");
