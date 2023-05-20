0e3ee71cacfcef967c7d01d4ca024d83036f0702
jihoon1026 <53975530+jjinjun1026@users.noreply.github.com>
2023-05-20 23:18:12 +0900

    Add files via upload

---
+++ src/com/kmit/www/Main.java
@@ -0,0 +1,22 @@
+package com.kmit.www;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 0;
+        String b = "hello";
+        int c = 10;
+        double d = 1.4D;
+        String e = null;
+        int f = (int)d;
+        double g = 2.9;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+c);
+        System.out.println(a+d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
